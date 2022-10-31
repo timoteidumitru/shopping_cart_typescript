b811369a8d6dbae10f0ff0318d9285e81543f64a
Timotei Dumitru <dumitru.timy@gmail.com>
2022-10-31 15:58:04 +0000

    update server

---
+++ build/precache-manifest.380b546318d991b3cb37064b1494a258.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972f455f9b432e34a37c1ddbcf16d0a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72fe5406f84ff0ed6c29",
+    "url": "/static/js/2.ce09467a.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.ce09467a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6858f6ca5e5111eacb4f",
+    "url": "/static/js/main.37e8fe22.chunk.js"
+  },
+  {
+    "revision": "9ddfda547cabaa3e17c8",
+    "url": "/static/js/runtime-main.8b5f33d2.js"
+  }
+]);
