749df3bc00dbb5cdf39301bae5982d2c5797cc94
Timotei Dumitru <dumitru.timy@gmail.com>
2022-10-31 14:51:51 +0000

    change server path

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+// Accessing the path module
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 
 const { data, categories } = require('./data.js')
-
-const PORT = process.env.PORT
-
+const PORT = process.env.PORT || 3001
 const app = express()
 
+// middlewhere
+app.use(express.static(path.resolve(__dirname, './build')))
 app.use(cors())
+
+// routes
 
 app.get('/api/products/', function (req, res) {
   res.setHeader('Content-Type', 'application/json')
@@ -19,6 +23,10 @@ app.get('/api/product/categories/', function (req, res) {
   res.send(categories)
 })
 
-app.listen(3001 || PORT, () =>
+// app.get('*', function (request, response) {
+//   response.sendFile(path.resolve(__dirname, './build', 'index.html'))
+// })
+
+app.listen(PORT, () =>
   console.log(`Server successfully started on port ${PORT}!`)
 )
