34dbd9528ffd7cdd93c897105c17f96d0087d8d7
Timotei Dumitru <dumitru.timy@gmail.com>
2022-10-31 14:08:54 +0000

    change port

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 
-const { data, categories } = require('./data.js');
+const { data, categories } = require('./data.js')
 
-const app = express();
+const PORT = process.env.PORT
 
-app.use(cors());
+const app = express()
+
+app.use(cors())
 
 app.get('/api/products/', function (req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
-});
+  res.setHeader('Content-Type', 'application/json')
+  res.send(data)
+})
 
 app.get('/api/product/categories/', function (req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(categories);
-});
+  res.setHeader('Content-Type', 'application/json')
+  res.send(categories)
+})
 
-app.listen(3001, () => console.log('Server successfully started on port 3001!'));
+app.listen(3001 || PORT, () =>
+  console.log(`Server successfully started on port ${PORT}!`)
+)
